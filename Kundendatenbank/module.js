e3a331247db5371b14f6340a7610c29a4aa8109a
Rahman E <rahman.erturk@gmail.com>
2022-10-11 17:08:14 +0200

    Aufgabe Kundendatenbank gemacht

---
+++ Kundendatenbank/module.js
@@ -0,0 +1,443 @@
+const ipAndAvatar = [
+  {
+    avatar: "https://robohash.org/quivelcum.png?size=50x50&set=set1",
+    ip_address: "203.234.221.31",
+  },
+  {
+    avatar:
+      "https://robohash.org/consecteturrecusandaeillo.png?size=50x50&set=set1",
+    ip_address: "228.64.165.0",
+  },
+  {
+    avatar:
+      "https://robohash.org/sapientefugaexplicabo.png?size=50x50&set=set1",
+    ip_address: "237.144.160.67",
+  },
+  {
+    avatar: "https://robohash.org/occaecatiteneturad.png?size=50x50&set=set1",
+    ip_address: "251.46.198.181",
+  },
+  {
+    avatar: "https://robohash.org/quisnesciuntut.png?size=50x50&set=set1",
+    ip_address: "6.199.124.155",
+  },
+  {
+    avatar: "https://robohash.org/occaecatiquaeratquo.png?size=50x50&set=set1",
+    ip_address: "50.116.255.201",
+  },
+  {
+    avatar: "https://robohash.org/estinventoreeveniet.png?size=50x50&set=set1",
+    ip_address: "102.39.100.33",
+  },
+  {
+    avatar: "https://robohash.org/etliberoquaerat.png?size=50x50&set=set1",
+    ip_address: "100.46.8.14",
+  },
+  {
+    avatar: "https://robohash.org/dictadoloribusesse.png?size=50x50&set=set1",
+    ip_address: "49.88.115.201",
+  },
+  {
+    avatar: "https://robohash.org/eaqueuteligendi.png?size=50x50&set=set1",
+    ip_address: "225.49.203.142",
+  },
+  {
+    avatar: "https://robohash.org/quiisteveniam.png?size=50x50&set=set1",
+    ip_address: "203.89.47.40",
+  },
+  {
+    avatar:
+      "https://robohash.org/voluptatemaccusamusut.png?size=50x50&set=set1",
+    ip_address: "78.24.26.136",
+  },
+  {
+    avatar: "https://robohash.org/doloresautest.png?size=50x50&set=set1",
+    ip_address: "156.234.240.24",
+  },
+  {
+    avatar: "https://robohash.org/suntipsumquis.png?size=50x50&set=set1",
+    ip_address: "17.142.70.147",
+  },
+  {
+    avatar: "https://robohash.org/esseatquecum.png?size=50x50&set=set1",
+    ip_address: "127.161.47.84",
+  },
+  {
+    avatar: "https://robohash.org/mollitiaundeexpedita.png?size=50x50&set=set1",
+    ip_address: "58.251.80.221",
+  },
+  {
+    avatar: "https://robohash.org/perferendissitab.png?size=50x50&set=set1",
+    ip_address: "164.9.204.28",
+  },
+  {
+    avatar: "https://robohash.org/quidemvelet.png?size=50x50&set=set1",
+    ip_address: "179.27.10.149",
+  },
+  {
+    avatar:
+      "https://robohash.org/quaeratreprehenderitesse.png?size=50x50&set=set1",
+    ip_address: "51.190.118.134",
+  },
+  {
+    avatar: "https://robohash.org/inventoreutdolor.png?size=50x50&set=set1",
+    ip_address: "168.139.68.220",
+  },
+  {
+    avatar:
+      "https://robohash.org/quaeratmaioresrepellat.png?size=50x50&set=set1",
+    ip_address: "78.142.138.209",
+  },
+  {
+    avatar: "https://robohash.org/autdictanon.png?size=50x50&set=set1",
+    ip_address: "225.187.166.16",
+  },
+  {
+    avatar: "https://robohash.org/natusutut.png?size=50x50&set=set1",
+    ip_address: "92.236.156.225",
+  },
+  {
+    avatar:
+      "https://robohash.org/consequaturnisilaboriosam.png?size=50x50&set=set1",
+    ip_address: "150.32.103.97",
+  },
+  {
+    avatar: "https://robohash.org/voluptatemfugitnulla.png?size=50x50&set=set1",
+    ip_address: "213.96.192.47",
+  },
+  {
+    avatar: "https://robohash.org/porrocommodideserunt.png?size=50x50&set=set1",
+    ip_address: "107.35.165.130",
+  },
+  {
+    avatar: "https://robohash.org/facilisomnissaepe.png?size=50x50&set=set1",
+    ip_address: "124.85.226.254",
+  },
+  {
+    avatar: "https://robohash.org/suntvoluptasquaerat.png?size=50x50&set=set1",
+    ip_address: "83.120.146.39",
+  },
+  {
+    avatar: "https://robohash.org/ipsavelqui.png?size=50x50&set=set1",
+    ip_address: "68.17.147.142",
+  },
+  {
+    avatar: "https://robohash.org/sequiutest.png?size=50x50&set=set1",
+    ip_address: "106.170.41.251",
+  },
+  {
+    avatar:
+      "https://robohash.org/quisexplicabolaudantium.png?size=50x50&set=set1",
+    ip_address: "204.57.129.214",
+  },
+  {
+    avatar: "https://robohash.org/delenitibeataerem.png?size=50x50&set=set1",
+    ip_address: "88.102.17.212",
+  },
+  {
+    avatar:
+      "https://robohash.org/consequunturomnisconsequatur.png?size=50x50&set=set1",
+    ip_address: "152.212.211.197",
+  },
+  {
+    avatar: "https://robohash.org/explicaboenimtenetur.png?size=50x50&set=set1",
+    ip_address: "156.106.65.114",
+  },
+  {
+    avatar:
+      "https://robohash.org/aspernaturipsumeveniet.png?size=50x50&set=set1",
+    ip_address: "9.173.197.132",
+  },
+  {
+    avatar:
+      "https://robohash.org/similiqueasperioreset.png?size=50x50&set=set1",
+    ip_address: "52.254.203.214",
+  },
+  {
+    avatar:
+      "https://robohash.org/repellendusquidolorum.png?size=50x50&set=set1",
+    ip_address: "14.200.227.168",
+  },
+  {
+    avatar: "https://robohash.org/rationequiomnis.png?size=50x50&set=set1",
+    ip_address: "220.122.121.26",
+  },
+  {
+    avatar: "https://robohash.org/nequesuntaut.png?size=50x50&set=set1",
+    ip_address: "70.167.113.134",
+  },
+  {
+    avatar: "https://robohash.org/esseautest.png?size=50x50&set=set1",
+    ip_address: "202.181.116.87",
+  },
+  {
+    avatar: "https://robohash.org/etiustodelectus.png?size=50x50&set=set1",
+    ip_address: "176.122.52.72",
+  },
+  {
+    avatar: "https://robohash.org/sintcumeos.png?size=50x50&set=set1",
+    ip_address: "235.110.105.191",
+  },
+  {
+    avatar: "https://robohash.org/ullamquamsaepe.png?size=50x50&set=set1",
+    ip_address: "130.188.253.145",
+  },
+  {
+    avatar:
+      "https://robohash.org/nostrumnonvoluptatibus.png?size=50x50&set=set1",
+    ip_address: "84.221.184.181",
+  },
+  {
+    avatar: "https://robohash.org/nonsequipraesentium.png?size=50x50&set=set1",
+    ip_address: "136.206.105.248",
+  },
+  {
+    avatar: "https://robohash.org/earumautoccaecati.png?size=50x50&set=set1",
+    ip_address: "73.149.91.10",
+  },
+  {
+    avatar: "https://robohash.org/idabenim.png?size=50x50&set=set1",
+    ip_address: "36.70.253.158",
+  },
+  {
+    avatar: "https://robohash.org/abquiaunde.png?size=50x50&set=set1",
+    ip_address: "209.18.120.87",
+  },
+  {
+    avatar:
+      "https://robohash.org/nihilrationevoluptatibus.png?size=50x50&set=set1",
+    ip_address: "210.4.203.203",
+  },
+  {
+    avatar:
+      "https://robohash.org/consecteturdelectusvoluptatem.png?size=50x50&set=set1",
+    ip_address: "101.165.60.223",
+  },
+  {
+    avatar: "https://robohash.org/omnisquiinventore.png?size=50x50&set=set1",
+    ip_address: "183.185.233.138",
+  },
+  {
+    avatar: "https://robohash.org/sintmagninesciunt.png?size=50x50&set=set1",
+    ip_address: "180.26.122.94",
+  },
+  {
+    avatar: "https://robohash.org/laboriosamquisint.png?size=50x50&set=set1",
+    ip_address: "155.118.216.239",
+  },
+  {
+    avatar: "https://robohash.org/aliquamevenietquia.png?size=50x50&set=set1",
+    ip_address: "87.45.232.152",
+  },
+  {
+    avatar: "https://robohash.org/nondelectuseos.png?size=50x50&set=set1",
+    ip_address: "133.94.112.214",
+  },
+  {
+    avatar:
+      "https://robohash.org/doloribusrepellatillum.png?size=50x50&set=set1",
+    ip_address: "1.135.108.226",
+  },
+  {
+    avatar: "https://robohash.org/ipsamidquis.png?size=50x50&set=set1",
+    ip_address: "249.147.96.198",
+  },
+  {
+    avatar: "https://robohash.org/dictaarchitectoquos.png?size=50x50&set=set1",
+    ip_address: "255.90.89.145",
+  },
+  {
+    avatar: "https://robohash.org/errormolestiaequis.png?size=50x50&set=set1",
+    ip_address: "221.110.168.75",
+  },
+  {
+    avatar: "https://robohash.org/laborumevenietporro.png?size=50x50&set=set1",
+    ip_address: "106.85.210.228",
+  },
+  {
+    avatar: "https://robohash.org/etsedexercitationem.png?size=50x50&set=set1",
+    ip_address: "251.241.191.113",
+  },
+  {
+    avatar:
+      "https://robohash.org/quodrecusandaeblanditiis.png?size=50x50&set=set1",
+    ip_address: "180.147.187.63",
+  },
+  {
+    avatar: "https://robohash.org/etdoloreslabore.png?size=50x50&set=set1",
+    ip_address: "24.203.108.205",
+  },
+  {
+    avatar: "https://robohash.org/voluptatemdoloremex.png?size=50x50&set=set1",
+    ip_address: "60.202.129.156",
+  },
+  {
+    avatar:
+      "https://robohash.org/delenitirerumconsequuntur.png?size=50x50&set=set1",
+    ip_address: "131.237.174.135",
+  },
+  {
+    avatar: "https://robohash.org/quisquamprovidentet.png?size=50x50&set=set1",
+    ip_address: "204.243.147.217",
+  },
+  {
+    avatar:
+      "https://robohash.org/debitisquisquamquibusdam.png?size=50x50&set=set1",
+    ip_address: "148.59.87.54",
+  },
+  {
+    avatar: "https://robohash.org/temporeetest.png?size=50x50&set=set1",
+    ip_address: "105.117.247.187",
+  },
+  {
+    avatar: "https://robohash.org/autitaquemodi.png?size=50x50&set=set1",
+    ip_address: "54.128.66.74",
+  },
+  {
+    avatar:
+      "https://robohash.org/molestiaenobismolestiae.png?size=50x50&set=set1",
+    ip_address: "236.176.74.199",
+  },
+  {
+    avatar:
+      "https://robohash.org/distinctiocorruptiest.png?size=50x50&set=set1",
+    ip_address: "153.184.99.182",
+  },
+  {
+    avatar: "https://robohash.org/molestiaealiquamea.png?size=50x50&set=set1",
+    ip_address: "145.215.97.181",
+  },
+  {
+    avatar: "https://robohash.org/deseruntetomnis.png?size=50x50&set=set1",
+    ip_address: "28.67.237.206",
+  },
+  {
+    avatar: "https://robohash.org/repellendusexeos.png?size=50x50&set=set1",
+    ip_address: "223.96.188.70",
+  },
+  {
+    avatar: "https://robohash.org/animiipsaasperiores.png?size=50x50&set=set1",
+    ip_address: "126.194.94.184",
+  },
+  {
+    avatar: "https://robohash.org/exquasitaque.png?size=50x50&set=set1",
+    ip_address: "248.149.173.82",
+  },
+  {
+    avatar:
+      "https://robohash.org/consequuntursuntdolores.png?size=50x50&set=set1",
+    ip_address: "241.1.77.100",
+  },
+  {
+    avatar: "https://robohash.org/suntpraesentiumin.png?size=50x50&set=set1",
+    ip_address: "139.246.53.96",
+  },
+  {
+    avatar: "https://robohash.org/etmolestiaseos.png?size=50x50&set=set1",
+    ip_address: "98.251.252.65",
+  },
+  {
+    avatar: "https://robohash.org/earumeumodio.png?size=50x50&set=set1",
+    ip_address: "0.13.7.200",
+  },
+  {
+    avatar: "https://robohash.org/esseeamaxime.png?size=50x50&set=set1",
+    ip_address: "53.190.156.131",
+  },
+  {
+    avatar: "https://robohash.org/minusetipsum.png?size=50x50&set=set1",
+    ip_address: "206.114.156.8",
+  },
+  {
+    avatar: "https://robohash.org/incidunttemporeest.png?size=50x50&set=set1",
+    ip_address: "207.195.223.247",
+  },
+  {
+    avatar: "https://robohash.org/itaquevoluptatemest.png?size=50x50&set=set1",
+    ip_address: "81.166.217.134",
+  },
+  {
+    avatar:
+      "https://robohash.org/oditadipiscivoluptatem.png?size=50x50&set=set1",
+    ip_address: "243.116.188.20",
+  },
+  {
+    avatar:
+      "https://robohash.org/porrovoluptatibusmolestiae.png?size=50x50&set=set1",
+    ip_address: "209.50.123.132",
+  },
+  {
+    avatar:
+      "https://robohash.org/beataeinnecessitatibus.png?size=50x50&set=set1",
+    ip_address: "177.193.160.218",
+  },
+  {
+    avatar:
+      "https://robohash.org/etconsequatursapiente.png?size=50x50&set=set1",
+    ip_address: "209.244.121.79",
+  },
+  {
+    avatar: "https://robohash.org/nonarepudiandae.png?size=50x50&set=set1",
+    ip_address: "224.20.186.246",
+  },
+  {
+    avatar: "https://robohash.org/doloreveniammollitia.png?size=50x50&set=set1",
+    ip_address: "135.14.1.209",
+  },
+  {
+    avatar: "https://robohash.org/quianimia.png?size=50x50&set=set1",
+    ip_address: "133.200.36.7",
+  },
+  {
+    avatar:
+      "https://robohash.org/eligendicorruptideleniti.png?size=50x50&set=set1",
+    ip_address: "95.69.101.6",
+  },
+  {
+    avatar: "https://robohash.org/eumsitest.png?size=50x50&set=set1",
+    ip_address: "64.89.221.147",
+  },
+  {
+    avatar: "https://robohash.org/quiaetplaceat.png?size=50x50&set=set1",
+    ip_address: "204.86.208.141",
+  },
+  {
+    avatar: "https://robohash.org/utculpavoluptate.png?size=50x50&set=set1",
+    ip_address: "33.241.230.255",
+  },
+  {
+    avatar: "https://robohash.org/nonipsammolestiae.png?size=50x50&set=set1",
+    ip_address: "250.238.18.110",
+  },
+  {
+    avatar:
+      "https://robohash.org/culpadignissimosdoloribus.png?size=50x50&set=set1",
+    ip_address: "100.59.133.253",
+  },
+  {
+    avatar: "https://robohash.org/dolorefugitesse.png?size=50x50&set=set1",
+    ip_address: "178.17.207.31",
+  },
+  {
+    avatar: "https://robohash.org/animidoloribuseius.png?size=50x50&set=set1",
+    ip_address: "44.57.143.138",
+  },
+  {
+    avatar: "https://robohash.org/enimoccaecatiest.png?size=50x50&set=set1",
+    ip_address: "1.149.87.112",
+  },
+];
+
+const randomIP = () => {
+  const random = () => Math.floor(Math.random() * ipAndAvatar.length);
+
+  return ipAndAvatar[random()].ip_address;
+};
+
+const randomAvatar = () => {
+  const random = () => Math.floor(Math.random() * ipAndAvatar.length);
+
+  return ipAndAvatar[random()].avatar;
+};
+
+export { randomIP, randomAvatar };
