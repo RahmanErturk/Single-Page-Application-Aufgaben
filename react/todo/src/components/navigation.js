c4354573093f66269851258dba6859b53c0ef718
Rahman E <rahman.erturk@gmail.com>
2022-10-18 14:38:20 +0200

    css und scss angewendet

--- react/todo/src/components/navigation.js
+++ react/todo/src/components/navigation.js
@@ -1,11 +1,12 @@
+import "./Navigation.scss";
 import NavItem from "./NavItem";
 
 // Ein Komponent fängt immer mit einer großen Buchstabe an.
 
 function Navigation() {
   return (
-    <nav>
-      <ul>
+    <nav className="Navigation">
+      <ul className="Navigation__List">
         <NavItem navLabel="Link 1" navLink="https://google.de" />
         <NavItem navLabel="Link 2" navLink="https://google.de" />
         <NavItem navLabel="Link 3" navLink="https://google.de" />
