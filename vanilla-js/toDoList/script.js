aeae26b1bdb5ee115a526113e5d1c6897d9cbcf1
Rahman E <rahman.erturk@gmail.com>
2022-10-23 12:41:22 +0200

    die Daten im Local-Storage gespeichert

--- vanilla-js/toDoList/script.js
+++ vanilla-js/toDoList/script.js
@@ -1,42 +1,77 @@
 const taskList = document.querySelector(".task-list");
 const form = document.querySelector(".adding-task");
 const formInput = document.getElementById("adding-task-input");
+const resetBtn = document.getElementById("tasks-reset_btn");
+let removeButtons;
+let checkboxes;
 
-const tasks = [];
+function addHTML(todo) {
+  const taskListItem = document.createElement("li");
+  taskListItem.classList.add("task-list-item");
 
-const event = form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  if (formInput.value !== "") {
-    const taskListItem = document.createElement("li");
-    taskListItem.classList.add("task-list-item");
+  const listItemContainer = document.createElement("div");
+  listItemContainer.classList.add("list-item-container");
 
-    const listItemContainer = document.createElement("div");
-    listItemContainer.classList.add("list-item-container");
+  const checkContainer = document.createElement("div");
+  checkContainer.classList.add("check-container");
 
-    const listItemContent = document.createElement("p");
-    listItemContent.classList.add("list-item-content");
+  const listItemContent = document.createElement("p");
+  listItemContent.classList.add("list-item-content");
 
-    const checkContainer = document.createElement("div");
-    checkContainer.classList.add("check-container");
+  const listItemCheck = document.createElement("input");
+  listItemCheck.type = "checkbox";
+  listItemCheck.checked = todo.isCompleted;
+  listItemCheck.classList.add("todoCheckBox");
 
-    const listItemCheck = document.createElement("input");
-    listItemCheck.type = "checkbox";
+  const btnsContainer = document.createElement("div");
+  btnsContainer.classList.add("btns-container");
 
-    const listItemRemove = document.createElement("button");
-    listItemRemove.classList.add("remove-btn");
-    listItemRemove.innerText = "Remove";
+  const listItemRemove = document.createElement("button");
+  listItemRemove.classList.add("remove-btn");
+  listItemRemove.innerText = "Remove";
 
-    taskList.append(taskListItem);
-    taskListItem.append(listItemContainer);
-    listItemContainer.append(checkContainer, listItemRemove);
-    checkContainer.append(listItemCheck, listItemContent);
+  taskList.append(taskListItem);
+  taskListItem.append(listItemContainer);
+  listItemContainer.append(checkContainer, btnsContainer);
+  checkContainer.append(listItemCheck, listItemContent);
+  btnsContainer.append(listItemRemove);
 
-    listItemContent.innerText = formInput.value;
-    form.reset();
+  listItemContent.innerText = todo.text;
+}
 
-    listItemRemove.addEventListener("click", () => {
-      taskListItem.remove();
+const startConfig = () => {
+  // initial settings
+
+  const todos = JSON.parse(localStorage.getItem("todos"));
+  if (!todos) {
+    localStorage.setItem("todos", JSON.stringify([]));
+  } else {
+    todos.forEach((todo) => {
+      addHTML(todo);
     });
+    removeButtons = document.querySelectorAll(".remove-btn");
+    checkboxes = document.querySelectorAll(".todoCheckBox");
+  }
+};
+
+startConfig();
+
+const addTask = (event) => {
+  event.preventDefault();
+
+  if (formInput.value !== "") {
+    const todo = {
+      text: formInput.value,
+      isCompleted: false,
+    };
+
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+    // console.log(todos);
+
+    addHTML(todo);
+    form.reset();
   } else {
     const warning = document.createElement("p");
     warning.innerText = "You should type a task!!!";
@@ -47,4 +82,46 @@ const event = form.addEventListener("submit", (event) => {
       warning.remove();
     });
   }
-});
+};
+
+const removeTodo = (e) => {
+  const removeTodoListItem = e.target.parentElement.parentElement.parentElement;
+  console.log(removeTodoListItem);
+  const text = removeTodoListItem.firstChild.firstChild.children[1].textContent;
+  console.log(text);
+
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  todos = todos.filter((td) => td.text != text);
+  localStorage.setItem("todos", JSON.stringify(todos));
+
+  removeTodoListItem.remove();
+};
+
+const completeTodo = (e) => {
+  const checkTodoListItem = e.target.parentElement;
+  console.log(checkTodoListItem);
+  const text = checkTodoListItem.children[1].textContent;
+  console.log(text);
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  todos.forEach((td) =>
+    td.text === text ? (td.isCompleted = !td.isCompleted) : td.isCompleted
+  );
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const resetAllTasks = (e) => {
+  let allListItems =
+    e.target.parentElement.parentElement.children[1].childNodes;
+  for (let index = 0; index < allListItems.length; index++) {
+    allListItems[index].remove();
+  }
+
+  let todos = JSON.parse(localStorage.getItem("todos"));
+  todos = [];
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+form.addEventListener("submit", addTask);
+removeButtons.forEach((btn) => btn.addEventListener("click", removeTodo));
+checkboxes.forEach((check) => check.addEventListener("click", completeTodo));
+resetBtn.addEventListener("click", resetAllTasks);
